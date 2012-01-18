960470ffc9f7803720d7b40f211e536aa5adfc4e
Denis Nesterov <cireto@gmail.com>
2012-01-18 19:27:22 +0400

    nifi-load/save

--- image.py
+++ image.py
@@ -1,10 +1,10 @@
 # Nifti Image load and save
 import numpy as np
 import nibabel as nib
-import os
+from os import *
 
 def loadnii(folder,name):
-    
+    """ Load Nifti file and parse it to image and header """
     img = nib.load(os.path.join(folder,name))
     data = img.get_data()
     hdr = img.get_header()
@@ -18,3 +18,16 @@ def convert(img,folder):
     a=nib.funcs.four_to_three(img)
     for i in range(len(a)):
     nib.nifti1.save(a[i],folder+'%s'%(i))
+
+def dcm_parser(folder,folder_out=[]):
+    if folder_out == []:
+        folder_out=folder
+    import dicom
+    ls=listdir(folder)
+    for i in ls:
+        ds=dicom.read_file(path.join(folder,i))
+        if ds.PatientsName in listdir(folder_out):
+            if ds.SeriesNumber in listdir(path.join(folder_out,ds.PatientsName)):
+                
+    
+    
