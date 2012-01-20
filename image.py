e44c3a6ae05baa31e1caf6396f8e16472e4f22f6
Denis Nesterov <cireto@gmail.com>
2012-01-20 17:57:31 +0400

    add dicom parser in image

--- image.py
+++ image.py
@@ -1,15 +1,14 @@
 # Nifti Image load and save
 import numpy as np
-import nibabel as nib
+#import nibabel as nib
 from os import *
-
+import timeit
 def loadnii(folder,name):
     """ Load Nifti file and parse it to image and header """
     img = nib.load(os.path.join(folder,name))
     data = img.get_data()
     hdr = img.get_header()
-	return hdr, data
-    
+    return hdr, data   
 def savenii(data,hdr,folder):
     nib.nifti1.save(nib.Nifti1Image(data, np.eye(4),hdr),folder)
 
@@ -17,17 +16,30 @@ def convert(img,folder):
 #convert 4d image
     a=nib.funcs.four_to_three(img)
     for i in range(len(a)):
-    nib.nifti1.save(a[i],folder+'%s'%(i))
+        nib.nifti1.save(a[i],folder+'%s'%(i))
 
 def dcm_parser(folder,folder_out=[]):
+    """ parse DICOM in folder and copy it in folder_out
+with folder structure /PatientName-BirthDate/StudyNumber/SeriesNumber/"""
+    import dicom
+    import shutil
+    
     if folder_out == []:
         folder_out=folder
-    import dicom
     ls=listdir(folder)
     for i in ls:
-        ds=dicom.read_file(path.join(folder,i))
-        if ds.PatientsName in listdir(folder_out):
-            if ds.SeriesNumber in listdir(path.join(folder_out,ds.PatientsName)):
-                
+        try:
+            ds=dicom.read_file(path.join(folder,i))
+            out_path=path.join(folder_out,ds.PatientsName,str(ds.StudyDate)+'_'+str(ds.StudyID),str(ds.SeriesNumber))
+            shutil.copy(path.join(folder,i),out_path+'/')
+        except IOError as (s):
+                makedirs(s.filename)
+                shutil.copy(path.join(folder,i),out_path+'/')
+        continue
+    print len(listdir(folder))
+    print len(listdir(out_path))
+     
     
-    
+func=dcm_parser('/media/63A0113C6D30EAE8/_PERF/temp/perf_20111228_111309207/','/media/63A0113C6D30EAE8/_CT/')
+t = timeit.Timer(stmt="func", setup="from __main__ import * ")
+print t.timeit()
