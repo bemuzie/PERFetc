5ce65405ca3326b968d12da5415a000e3c9ee7d7
Denis Nesterov_home <cireto@gmail.com>
2012-01-25 04:52:18 +0400

    TIPS4d filter finished

--- testTIPS4.py
+++ testTIPS4.py
@@ -1,12 +1,14 @@
 __author__ = 'ct'
 import tips4,image
 import numpy as np
-
+from timeit import timeit
 img,hdr=image.loadnii('/media/data/_PERF/Rogachevskiy','GeneralBodyPerfusionROGACHEVSKIJVF10031945.nii')
-img=image.crop(img,256,256,160,100)
+img=image.crop(img,256,256,160,20)
 #img=np.arange(6*6*4*10).reshape(6,6,4,10)
 print np.shape(image)
 dim_c=3
 sigG=2
+
 a=tips4.convolve4d(img,3,1,10000000)
-image.savenii(a,hdr,'/media/data/_PERF/Rogachevskiy/TIPS.nii')
+#image.savenii(a,hdr,'/media/data/_PERF/Rogachevskiy/TIPS.nii')
+print (timeit('tips4.convolve4d(img,3,1,10000000)', 'from __main__ import *', number = 10))
--- tips4.py
+++ tips4.py
@@ -4,18 +4,24 @@
 in 4D CT perfusion scans produces high-quality cerebral blood flow maps"
 """
 import numpy as np
-from scipy import ndimage
-def gauss_cl(x,sigma):
-    """Gaussian clousness function. x and y should be Cartessian coordinates"""
-    differ=np.array(x)-np.array([1,1,1])
-    euclid=np.sqrt(np.sum(differ*differ))
-    return np.exp(-0.5*(euclid/sigma)*(euclid/sigma))
+def gauss_kernel(size,sigma):
+    """Gaussian symetric 4d clousnes kernel """
+    kern=np.ones((size,size,size))
+    iterArray=kern.copy()
+
+    for i,val in np.ndenumerate(iterArray):
+        differ=np.array(i)-np.array([size/2,size/2,size/2])
+        euclid=np.sqrt(np.sum(differ*differ))
+        kern[i]= np.exp(-0.5*(euclid/sigma)*(euclid/sigma))
+    kern=kern[:,:,:,None]
+    return kern
 def TimeProfile_cl(x,y,sigma):
     """Time profile clousness function. x and y should have shape= (1,1,1,len(time))"""
     diff=x-y
-    SSD=np.sum(diff*diff)/len(x)
-    return np.exp(-0.5*(SSD/sigma)*(SSD/sigma))
-
+    SSD=np.sum(diff*diff,axis=-1)/len(x)
+    kern=np.exp(-0.5*(SSD/sigma)*(SSD/sigma))
+    kern=kern[:,:,:,None]
+    return kern
 
 def convolve4d(img,size,sigG,sigT):
     """Convolve 4d array with symmetric 4d kernel with size "dim_c" through all temporal axis.
@@ -23,44 +29,35 @@ def convolve4d(img,size,sigG,sigT):
     """
     est=np.prod(np.asarray(np.shape(img[:,:,:,0]))-size+1)
     made=0
-    print type(img)
     sigG=float(sigG)
     sigT=float(sigT)
+
     #if kernel size even break it
     if size%2 == 0:
         raise NameError('kernel should have odd size!!!')
     #
-    size_3d=tuple((size,size,size))
     size_half=int(size)/2
-    #
-    GausKern=np.ones(size_3d)
-    iterArray=GausKern.copy()
-    #
-    for i,val in np.ndenumerate(iterArray):
-        GausKern[i]=gauss_cl(i,sigG)
-    GausKern=GausKern[:,:,:,np.newaxis]
+    #Calculating Gaussian closeness function
+    GausKern=gauss_kernel(size,sigG)
     #
     out=np.zeros((np.shape(img)))
     #making iterator which don't contain borders
-    it = np.nditer (img[size_half:-size_half,size_half:-size_half,size_half:-size_half,0], flags=['multi_index'])
+    it = np.nditer (img[size_half:-size_half,size_half:-size_half,size_half:-size_half,0], flags=['c_index','multi_index'   ])
+    summing = np.sum
     while not it.finished:
+
         #determing cordinates of central pixel
-        cent=it.multi_index
-        x=cent[0]
-        y=cent[1]
-        z=cent[2]
-        center=tuple((x+size_half,y+size_half,z+size_half))
-        endboder=size
+        x,y,z=it.multi_index
+        center=(x+size_half,y+size_half,z+size_half)
         #Taking kernel of a volume
-        kernel = img[x:x+endboder,y:y+endboder,z:z+endboder]
+        kernel = img[x:x+size,y:y+size,z:z+size]
         #Calculating Time profile closeness function
         tp=TimeProfile_cl(img[center],kernel,sigT)
+        #print kernel.shape,GausKern.shape,tp.shape
+        coef=tp*GausKern
+        out[center]=summing(summing(summing(  coef*kernel,axis=0  ),axis=0),axis=0)/summing(coef)
 
-        #Calculating Gaussian closeness function
-
-        out[center]=np.sum(np.sum(np.sum(  kernel*tp*GausKern,axis=0  ),axis=0),axis=0)/np.sum(tp*GausKern)
-
-        made+=1
-        print made,'из',est
+#        made+=1
+#        print made,'из',est
         it.iternext()
     return out
