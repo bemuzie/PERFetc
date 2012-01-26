98410827d56280cf651146635a085cc104970cb1
Denis Nesterov <cireto@gmail.com>
2012-01-26 18:30:40 +0400

    little optimization of TIPS4d

--- test2.py
+++ test2.py
@@ -1,3 +1,3 @@
 __author__ = 'ct'
 from image import dcm_parser
-dcm_parser('/media/63A0113C6D30EAE8/_PERF/temp/240112_20120125_183904216/','/media/63A0113C6D30EAE8/_PERF')
+dcm_parser('/media/63A0113C6D30EAE8/_PERF/temp/240112_20120126_104956866/','/media/63A0113C6D30EAE8/_PERF')
--- testTIPS4.py
+++ testTIPS4.py
@@ -2,13 +2,21 @@ __author__ = 'ct'
 import tips4,image
 import numpy as np
 from timeit import timeit
-img,hdr=image.loadnii('/media/63A0113C6D30EAE8/_PERF/YAVNIK/Nifti4d','GeneralBodyPerfusionYAVNIKGA12081948s008a001_FC18.nii')
-img=image.crop(img,256,256,160,80)
+
+
+adress='/media/63A0113C6D30EAE8/_PERF/YAVNIK/Nifti4d'
+file='GeneralBodyPerfusionYAVNIKGAs005a001_FC17reg.nii'
+img,hdr=image.loadnii(adress,file)
+img=image.crop(img,256,256,160,20)
 #img=np.arange(50*50*40*17).reshape(50,50,40,17)
 print np.shape(image)
-dim_c=3
-sigG=2
 
-a=tips4.convolve4d(img,13,3,100)
-image.savenii(img,hdr,'/media/63A0113C6D30EAE8/_PERF/YAVNIK/TIPS_FC18_13_3_100.nii')
+a=np.linspace(100,1500,15)
+for i in a:
+    size=9
+    sigmaG=1.8
+    sigmaT=i
+
+    tips=tips4.convolve4d(img,size,sigmaG,sigmaT)
+    image.savenii(tips,hdr,'/media/63A0113C6D30EAE8/_PERF/YAVNIK/OLD_TIPS_%s_%s_%s_%s.nii'%(file[:-4],size,sigmaG,sigmaT))
 #print (timeit('tips4.convolve4d(img,13,3,100)', 'from __main__ import *', number = 10))
