ee1081090632ffe0375d12c2451280e89ef45dfd
Denis Nesterov_home <cireto@gmail.com>
2012-01-30 00:11:21 +0400

    SD across time added

--- testTIPS4.py
+++ testTIPS4.py
@@ -10,11 +10,12 @@ filelist=os.listdir('/media/WORK/_PERF/DZEVANOVSKII S.I. 30.12.1947/Nifti4d')
 adress='/media/WORK/_PERF/DZEVANOVSKII S.I. 30.12.1947/Nifti4d'
 adress_out='/media/WORK/_PERF/DZEVANOVSKII S.I. 30.12.1947/filtered/'
 fileexct='GeneralBodyPerfusionDZEVANOVSKIISI30121947_3_FC17_QDS.nii'
+croppart=16
 for file in filelist[:]:
     print file
     img,hdr, mrx=image.loadnii(adress,file)
     shp=np.shape(img)
-    img=image.crop(img,shp[0]/2,shp[1]/2,shp[2]/2,shp[2]/3)
+    img=image.crop(img,shp[0]/2,shp[1]/2,shp[2]/2,shp[2]/croppart)
 #img=np.arange(50*50*40*17).reshape(50,50,40,17)
     print np.shape(img)
     tips=img
@@ -22,15 +23,15 @@ for file in filelist[:]:
     sigmaG='along'
     sigmaT='Time'
 
-    """
-a=[900]
-for i in a:
-    size=7
-    sigmaG=1
-    sigmaT=i
+np.matrix.sum()
+    a=[400,700,1500]
+    for i in a:
+        size=9
+        sigmaG=1.5
+        sigmaT=i
 
-    tips=tips4.convolve4d(img,size,sigmaG,sigmaT)
-    """
-    tips=filters.std(img,13)
-    image.savenii(tips,mrx,adress_out+'%s_%s_%s_%s.nii'%(file[:-4],size,sigmaG,sigmaT))
+        tips=tips4.convolve4d(img,size,sigmaG,sigmaT)
+
+#    tips=filters.std(img,13)
+        image.savenii(tips,mrx,adress_out+'%s_%s_%s_%s_crp%s.nii'%(file[:-4],size,sigmaG,sigmaT,croppart))
 #print (timeit('tips4.convolve4d(img,13,3,100)', 'from __main__ import *', number = 10))
