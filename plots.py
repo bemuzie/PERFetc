774794aba91e049598b90f3f8972dd6f216d44d6
Denis Nesterov_home <cireto@gmail.com>
2012-01-30 07:05:21 +0400

    smth changed

--- plots.py
+++ plots.py
@@ -31,20 +31,26 @@ p_popt[4]+=4
 p_popt[3]-=1
 
 bfr_p=curves.logn(TimeCont,p_popt[0],p_popt[1],p_popt[2],p_popt[3],p_popt[4])
-
+bfr_p_n=curves.logn(Time,p_popt[0],p_popt[1],p_popt[2],p_popt[3],p_popt[4])\
++np.random.normal(scale=9,size=len(Time))
 l_popt[2]+=0.001
 l_popt[0]-=100
 l_popt[4]+=4
 l_popt[3]+=2
 
 bfr_l=curves.logn(TimeCont,l_popt[0],l_popt[1],l_popt[2],l_popt[3],l_popt[4])
-
-plt.plot(Time,pancreas,'o--',TimeCont,pancreas_fitted)
-plt.plot(Time,lesion,'o--',TimeCont,lesion_fitted)
-
-plt.plot(TimeCont,bfr_l)
-plt.plot(TimeCont,bfr_p)
+bfr_l_n=curves.logn(Time,l_popt[0],l_popt[1],l_popt[2],l_popt[3],l_popt[4])\
++np.random.normal(scale=7,size=len(Time))
 
 
+plt.figure(facecolor='b')
+plt.axes(axisbg='k')
+plt.plot(Time,pancreas,'o--g',TimeCont,pancreas_fitted,'g',label='25.01')
+plt.plot(Time,lesion,'o--b',TimeCont,lesion_fitted,'b',label='25.01')
+plt.plot(Time,bfr_l_n,'o--r',TimeCont,bfr_l,'r',label='05.12')
+plt.plot(Time,bfr_p_n,'o--y',TimeCont,bfr_p,'y',label='05.12')
+leg=plt.legend(loc='upper left')
+frame  = leg.get_frame()
+frame.set_facecolor('0.5')
 plt.show()
 
