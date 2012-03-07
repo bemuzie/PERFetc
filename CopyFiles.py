8ef6edbf2121f1cb5b243a658a0077b44b52bb15
Denis Nesterov <cireto@gmail.com>
2012-03-07 14:15:13 +0400

    smth

--- CopyFiles.py
+++ CopyFiles.py
@@ -2,8 +2,8 @@ __author__ = 'denis'
 import image
 
 
-folder='/media/63A0113C6D30EAE8/_CT/temp/'
-out_folder='/media/63A0113C6D30EAE8/_CT/'
+folder='/media/63A0113C6D30EAE8/_PERF/temp/'
+out_folder='/media/63A0113C6D30EAE8/_PERF/'
 sfolder=dict(PatientsName=[0x10,0x10],StudyDate=[0x8,0x20],StudyID=[0x20,0x10],\
     SeriesNumber=[0x20,0x11],ConvolutionKernel=[0x18,0x1210],FilterType=[0x7005,0x100b])
 
