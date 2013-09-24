de74697a0654b61cc314ba290873711f1bf88adf
bemuzie <cireto@gmail.com>
2013-09-24 08:05:28 +0400

    cdx

--- image/pattern_finder.py
+++ image/pattern_finder.py
@@ -19,29 +19,35 @@ def isLesion(input_data,av_down,av_up,sd_down,sd_up):
 
 		
 		d_av=np.average(input_data)
-		d_sd=np.std(input_data)
+		
 
 		
 		if d_av > av_down and d_av<av_up:
-			
+			d_sd=np.std(input_data)
 			if d_sd > sd_down and d_sd < sd_up:
 				
 				return 1.
 			else: 
+				
 				return 0
 		else:
+			
 			return 0
+
 	except:
+		
 		print input_data
 		return 10
 
-IMAGE_FOLDER='D:\DICOM'
-IMAGE_NAME='IM00407.nii'
+IMAGE_FOLDER='/home/denest'
+IMAGE_NAME='13 Venous Phase  1.5  B30f-subvolume-scale_1.nii'
 img=nibabel.nifti1.load(os.path.join(IMAGE_FOLDER,IMAGE_NAME))
+hdr = img.get_header()
+affinemrx = hdr.get_sform()
 data=img.get_data()
 print np.array(data,dtype=float).shape
-data_new=generic_filter(np.array(data,dtype=float)[100:-100,100:-100,100:-100], isLesion , footprint=sphere_mask(1) ,extra_keywords={
-	'av_down':-10.,'av_up':10.,'sd_down':15.,'sd_up':25.})
+data_new=generic_filter(np.array(data,dtype=float), isLesion , footprint=sphere_mask(2) ,extra_keywords={
+	'av_down':148.,'av_up':152.,'sd_down':15,'sd_up':17.})
 
-img_new=nibabel.nifti1.Nifti1Image(data_new)
-nibabel.nifti1.save(img_new, os.path.join(IMAGE_FOLDER,IMAGE_NAME+'masked'))
+img_new=nibabel.nifti1.Nifti1Image(data_new,affinemrx)
+nibabel.nifti1.save(img_new, os.path.join(IMAGE_FOLDER,IMAGE_NAME[:-4]+'masked.nii'))
