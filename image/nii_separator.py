7ab7852b2377561f62de1fdce28f88e246112bbd
denest <cireto@gmail.com>
2014-06-05 21:28:53 +0400

    smth

--- image/nii_separator.py
+++ image/nii_separator.py
@@ -2,110 +2,110 @@ import nibabel as nib
 import os
 
 def separate_nii(input_file, output_folder=None):
-    """
-    Separate 4d *.nii file to 3d *.nii files and write them in output directory.
+	"""
+	Separate 4d *.nii file to 3d *.nii files and write them in output directory.
 
-    Args:
-      input_file (str): absolute path to 4d nii file
-      output_folder (str, optional): absolute path where 3d nii files will be saved. 
-	      If not given they will be saved in the same folder subfolder named like infut file.
+	Args:
+	  input_file (str): absolute path to 4d nii file
+	  output_folder (str, optional): absolute path where 3d nii files will be saved. 
+		  If not given they will be saved in the same folder subfolder named like infut file.
 
-    Returns:
-      True
+	Returns:
+	  True
 
-    """
-
-
-    file_name = os.path.basename(os.path.abspath(input_file)).split('.')[0] #naming output folder like file without extension
-    if not output_folder:
-    	output_folder = os.path.join(os.path.dirname(os.path.abspath(input_file)), file_name)
+	"""
 
 
-    vol4d = nib.load(os.path.abspath(input_file)) # loading 4d image
-    v4=vol4d.get_data()
-    hdr=vol4d.get_header()
-    print hdr
-    if vol4d.shape[-1]>1: #3d nii can have the forth dimension with length 1
+	file_name = os.path.basename(os.path.abspath(input_file)).split('.')[0] #naming output folder like file without extension
+	if not output_folder:
+		output_folder = os.path.join(os.path.dirname(os.path.abspath(input_file)), file_name)
+
+
+	vol4d = nib.load(os.path.abspath(input_file)) # loading 4d image
+	v4=vol4d.get_data()
+	hdr=vol4d.get_header()
+	print hdr
+	if vol4d.shape[-1]>1: #3d nii can have the forth dimension with length 1
 		
-        vol3d_list=nib.funcs.four_to_three(vol4d) # getting list of 3d volumes
-    else:
-        return True
+		vol3d_list=nib.funcs.four_to_three(vol4d) # getting list of 3d volumes
+	else:
+		return True
 
-    for i in range(len(vol3d_list)):
-    	try:
-        	nib.nifti1.save(vol3d_list[i], 
-        					os.path.join(output_folder, '%s_%s.nii.gz'%(file_name,i)))
-        except IOError, s:
-        	if s[0] == 2: #No directory exception
-        		os.mkdir(os.path.join(output_folder))
-        		nib.nifti1.save(vol3d_list[i], 
-        					os.path.join(output_folder, '%s_%s.nii.gz'%(file_name,i)))
-        		continue
+	for i in range(len(vol3d_list)):
+		try:
+			nib.nifti1.save(vol3d_list[i], 
+							os.path.join(output_folder, '%s_%s.nii.gz'%(file_name,i)))
+		except IOError, s:
+			if s[0] == 2: #No directory exception
+				os.mkdir(os.path.join(output_folder))
+				nib.nifti1.save(vol3d_list[i], 
+							os.path.join(output_folder, '%s_%s.nii.gz'%(file_name,i)))
+				continue
 
 
 
-    return True
+	return True
 
 def merge_nii(input_folder):
 	pass
 
 def get_resolution(input_file):
-    vol4d = nib.load(os.path.abspath(input_file))
-    hdr = vol4d.get_header()
-    return hdr.get_zooms()
+	vol4d = nib.load(os.path.abspath(input_file))
+	hdr = vol4d.get_header()
+	return hdr.get_zooms()
 
 def crop_image(input_file,x1,x2,y1,y2,z1,z2,output_file =None):
-    img = nib.load(os.path.abspath(input_file))
+	img = nib.load(os.path.abspath(input_file))
 
-    f = nib.load(os.path.abspath(input_file))
-    vol = f.get_data()
-    hdr = f.get_header()
-    res = hdr.get_zooms() # getting voxel size
-    mrx = hdr.get_sform() # getting affine matrix
-    print hdr
-    print mrx
-    print res
-    filtered_image=vol[-x2:-x1,-y2:-y1,-z2:-z1]
+	f = nib.load(os.path.abspath(input_file))
+	vol = f.get_data()
+	hdr = f.get_header()
+	res = hdr.get_zooms() # getting voxel size
+	mrx = hdr.get_sform() # getting affine matrix
+	print hdr
+	print mrx
+	print res
+	filtered_image=vol[-x2:-x1,-y2:-y1,-z2:-z1]
 
-    if output_file ==None:
-        output_file = os.path.join(os.path.dirname(os.path.abspath(input_file)))
-    
-    input_fname = os.path.basename(os.path.abspath(input_file)).split('.')[0]
-        
-    nii_im = nib.Nifti1Image(filtered_image, mrx,hdr)
+	if output_file ==None:
+		output_file = os.path.join(os.path.dirname(os.path.abspath(input_file)))
+	
+	input_fname = os.path.basename(os.path.abspath(input_file)).split('.')[0]
+		
+	nii_im = nib.Nifti1Image(filtered_image, mrx,hdr)
 
-    nib.nifti1.save(nii_im, 
-                        os.path.join(output_file, input_fname + "X%s_%s_Y%s_%s_Z%s_%s.nii"%(x1,x2,y1,y2,z1,z2)) )
+	nib.nifti1.save(nii_im, 
+					os.path.join(output_file, input_fname + "X%s_%s_Y%s_%s_Z%s_%s.nii"%(x1,x2,y1,y2,z1,z2)) )
 
 def get_crop_cords(roi_vol):
-	 """
-    return list of crop cordinates of the ROI volume.
+	"""
+	return list of crop cordinates of the ROI volume.
 
-    Args:
-      roi-vol (str): absolute path to 3d nii. values in nii should be 1for voxels wich are inside ROI and 0 for outside voxels
+	Args:
+		roi-vol (str): absolute path to 3d nii. values in nii should be 1for voxels wich are inside ROI and 0 for outside voxels
 
-    Returns:
-      [x1,x2,y1,y2,z1,z2] 
+	Returns:
+		[x1,x2,y1,y2,z1,z2] 
 
-    """
-	nii=nib.load(roi_vol).get_data()
+	"""
+	nii = nib.load(roi_vol).get_data()
 	borders = np.where(nii==1)
-	x1,y1,z1= [ np.min(i) for i in borders]
-	x2,y2,z2= [ np.min(i) for i in borders]
+	x1,y1,z1 = [ np.min(i) for i in borders]
+	x2,y2,z2 = [ np.min(i) for i in borders]
 	return [x1,x2,y1,y2,z1,z2]
 
 def set_header_from(image,image_header):
-    f1 = nib.load(os.path.abspath(image_header))
-    hdr = f1.get_header()
-    res = hdr.get_zooms() # getting voxel size
-    mrx = hdr.get_sform() # getting affine matrix
+	f1 = nib.load(os.path.abspath(image_header))
+	hdr = f1.get_header()
+	res = hdr.get_zooms() # getting voxel size
+	mrx = hdr.get_sform() # getting affine matrix
 
-    changing_image = nib.load(os.path.abspath(image))
-    d=changing_image.get_data()
-    
-    nib.nifti1.save(nib.Nifti1Image(d,mrx,hdr), image)
+	changing_image = nib.load(os.path.abspath(image))
+	d=changing_image.get_data()
+	
+	nib.nifti1.save(nib.Nifti1Image(d,mrx,hdr), image)
 
 if __name__ == '__main__':
-    set_header_from('/home/denest/PERF_volumes/TEMNOSAGATYI  A.V. 02.04.1973/ROI/pancreas_roi.nii.gz','/home/denest/PERF_volumes/TEMNOSAGATYI  A.V. 02.04.1973/NII/20140508_100402GeneralBodyPerfusionCopiedTEMNOSAGATYIAV02041973s004a001_filtered/20140508_100402GeneralBodyPerfusionCopiedTEMNOSAGATYIAV02041973s004a001_6_I40_G1.5.nii')
-    crop_image('/home/denest/PERF_volumes/TEMNOSAGATYI  A.V. 02.04.1973/ROI/pancreas_roi.nii.gz',
-                84,329,160,290,1,260)
+	set_header_from('/home/denest/PERF_volumes/TEMNOSAGATYI  A.V. 02.04.1973/ROI/pancreas_roi.nii.gz','/home/denest/PERF_volumes/TEMNOSAGATYI  A.V. 02.04.1973/NII/20140508_100402GeneralBodyPerfusionCopiedTEMNOSAGATYIAV02041973s004a001_filtered/20140508_100402GeneralBodyPerfusionCopiedTEMNOSAGATYIAV02041973s004a001_6_I40_G1.5.nii')
+	crop_image('/home/denest/PERF_volumes/TEMNOSAGATYI  A.V. 02.04.1973/ROI/pancreas_roi.nii.gz',
+				84,329,160,290,1,260)
--- workflow2.py
+++ workflow2.py
@@ -1,26 +1,44 @@
 #-*- coding: utf-8-*-
 import os
+import re
 import subprocess
 from image import nii_separator
 import nibabel as nib
 import numpy as np
 from filters import bilateral
+class AutoVivification(dict):
+    """Implementation of perl's autovivification feature."""
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
+
 class Dir_manager():
 	def __init__(self):
 		#self.abs_path = root_folder
 		#Creatioon of default folder structure
 		#self.dcm,self.nii,self.roi = [ os.pathjoin(self.abs_path,sf) for sf in ('DCM','ROI','NII')]
-		self.p = {}
+		self.p = AutoVivification()
+
 	def add_path(self,path_to,l,s=None,a=None,add_to=None):
 		if add_to:
 			path_to = os.path.join(add_to,path_to)
 		if not os.path.isdir(path_to) and not os.path.isfile(path_to):
 			os.makedirs(path_to)
-		self.p[l]={s:{a:path_to}}
+
+		self.p[l][s][a]=path_to
 	def get_path(self,l,s=None,a=None):
+		print l,s,a
 		return self.p[l][s][a]
-	def files_in(self,l,s=None):
-		[f for f in os.listdir(self.p[l][s]) if os.path.isfile(f)]
+	def files_in(self,l,s=None,a=None):
+		
+		target_folder = self.get_path(l,s)
+		return [f for f in os.listdir(target_folder) if os.path.isfile(os.path.join(target_folder,f))]
+	def print_all(self):
+		for i,v in self.p.items():
+			print i,v
 		
 
 			
@@ -31,6 +49,7 @@ class Dir_manager():
 
 
 #Parse DICOM and move them to DATA_STORAGE folder with generated subfolder structure /Patient_name/DCM/Examination_date/Series_Kernel_Filter
+
 pat=Dir_manager()
 
 pat.add_path('/home/denest/PERF_volumes/TEMNOSAGATYI  A.V. 02.04.1973','root')
@@ -65,33 +84,33 @@ dcm2nii HELP
 -x Reorient and crop 3D NIfTI images: Y,N = N
 """
 
+def convert_dcm_to_nii():
+	DCM2NII_PATH = '/home/denest/mricron/dcm2nii'
+	ini_path = 'dcm2nii.ini'
+	print pat.get_path('nii_raw')
+	dcm2nii_pars =['-4 y',#Create 4D volumes, else DTI/fMRI saved as many 3D volumes: Y,N = Y
+				 '-a n',# Anonymize [remove identifying information]: Y,N = Y
+				 '-b dcm2nii.ini',#load settings from specified inifile, e.g. '-b C:\set\t1.ini'  
+				 #'-c y',#Collapse input folders: Y,N = Y
+				 '-d n',#Date in filename [filename.dcm -> 20061230122032.nii]: Y,N = Y
+				 '-e y',# events (series/acq) in filename [filename.dcm -> s002a003.nii]: Y,N = Y
+				 '-f n',# Source filename [e.g. filename.par -> filename.nii]: Y,N = N
+				 '-g y',#gzip output, filename.nii.gz [ignored if '-n n']: Y,N = Y
+				 #'-i y',#ID  in filename [filename.dcm -> johndoe.nii]: Y,N = N
+				 #'-m n',#manually prompt user to specify output format [NIfTI input only]: Y,N = Y
+				 '-n y',#output .nii file [if no, create .hdr/.img pair]: Y,N = Y
+				 "-o '%s'"%pat.get_path('nii_raw'),#Output Directory, e.g. 'C:\TEMP' (if unspecified, source directory is used)
+				 '-p n',#Protocol in filename [filename.dcm -> TFE_T1.nii]: Y,N = Y
+				 '-r n',# Reorient image to nearest orthogonal: Y,N 
+				 '-s n',#SPM2/Analyze not SPM5/NIfTI [ignored if '-n y']: Y,N = N
+				 '-v y',#Convert every image in the directory: Y,N = Y
+				 '-x n',#Reorient and crop 3D NIfTI images: Y,N = N
+				 "'%s'"%pat.get_path('dcm') #INPUT FOLDER
+				]
 
-DCM2NII_PATH = '/home/denest/mricron/dcm2nii'
-ini_path = 'dcm2nii.ini'
-print pat.get_path('nii_raw')
-dcm2nii_pars =['-4 y',#Create 4D volumes, else DTI/fMRI saved as many 3D volumes: Y,N = Y
-			 '-a n',# Anonymize [remove identifying information]: Y,N = Y
-			 '-b dcm2nii.ini',#load settings from specified inifile, e.g. '-b C:\set\t1.ini'  
-			 #'-c y',#Collapse input folders: Y,N = Y
-			 '-d n',#Date in filename [filename.dcm -> 20061230122032.nii]: Y,N = Y
-			 '-e y',# events (series/acq) in filename [filename.dcm -> s002a003.nii]: Y,N = Y
-			 '-f n',# Source filename [e.g. filename.par -> filename.nii]: Y,N = N
-			 '-g y',#gzip output, filename.nii.gz [ignored if '-n n']: Y,N = Y
-			 #'-i y',#ID  in filename [filename.dcm -> johndoe.nii]: Y,N = N
-			 #'-m n',#manually prompt user to specify output format [NIfTI input only]: Y,N = Y
-			 '-n y',#output .nii file [if no, create .hdr/.img pair]: Y,N = Y
-			 "-o '%s'"%pat.get_path('nii_raw'),#Output Directory, e.g. 'C:\TEMP' (if unspecified, source directory is used)
-			 '-p n',#Protocol in filename [filename.dcm -> TFE_T1.nii]: Y,N = Y
-			 '-r n',# Reorient image to nearest orthogonal: Y,N 
-			 '-s n',#SPM2/Analyze not SPM5/NIfTI [ignored if '-n y']: Y,N = N
-			 '-v y',#Convert every image in the directory: Y,N = Y
-			 '-x n',#Reorient and crop 3D NIfTI images: Y,N = N
-			 "'%s'"%pat.get_path('dcm') #INPUT FOLDER
-			]
-"""
-subprocess.check_call(' '.join([DCM2NII_PATH,' '.join(dcm2nii_pars)] ),
-						shell=True)
-"""
+	subprocess.check_call(' '.join([DCM2NII_PATH,' '.join(dcm2nii_pars)] ),
+							shell=True)
+
 
 
 #compress DICOMs
@@ -131,10 +150,10 @@ x_to,y_to,z_to = map(np.max,borders_vol)
 pat.add_path('FILTERED_4','filtered',4,add_to=pat.get_path('nii'))
 
 
-
-for p,d,f in os.walk(pat.get_path('separated',4)):
-	for fname in f:
-		bilateral.bilateral(os.path.join(p,fname),output_folder=pat.get_path('filtered',4), sig_i=40,sig_g=1.5,x_range=[x_fr,x_to], y_range=[y_fr,y_to], z_range=[z_fr,z_to])
+def filter_vols():
+	for p,d,f in os.walk(pat.get_path('separated',4)):
+		for fname in f:
+			bilateral.bilateral(os.path.join(p,fname),output_folder=pat.get_path('filtered',4), sig_i=40,sig_g=1.5,x_range=[x_fr,x_to], y_range=[y_fr,y_to], z_range=[z_fr,z_to])
 
 
 #make / 
@@ -142,68 +161,78 @@ for p,d,f in os.walk(pat.get_path('separated',4)):
 #Manual manipulations
 #Create ROIs for aorta,IVC
 #Choose target phase and make registration
+TARGET_PHASE = 4
+pat.add_path('s004a001_10_I40_G1_registration_roi.nii.gz','registration_roi',add_to=pat.get_path('roi'))
+
+pat.add_path('Registered_4','reg',4,add_to=pat.get_path('nii'))
 #Create ROIs for pancreas,tumor,tumor1
 #Create registration mask
-for fn in pat.flist('filtered',4):
-	 s,a=fn.split('.','s','a')[0:2]
-	pat.ap('filtered',s,a)
+print pat.files_in('filtered',4)
+for fn in pat.files_in('filtered',4):
+	splited_fname = re.split(r"s|a|_",fn)
+	print splited_fname
+	s,a = [int(i) for i in (splited_fname[1],splited_fname[3])  ]
 	
-	pat.add_path('filtered',s,a,join_to(pat.get_path('filtered'))
-
-
-ANTs_PATH = '/home/denest/ANTs-1.9.x-Linux/bin/'
-TARGET_PHASE =8
-MASK = ''
-#registration
-
-WORKING_FOLDER = pat.gp('root')
-images_folder = pat.gp('filtered,4')
-fixed_im=pat.get_path('filtered',4,4)
-#moved_im='20140508_100402GeneralBodyPerfusionCopiedTEMNOSAGATYIAV02041973s004a001_20_I40_G1.5-subvolume-scale_1.nii.gz'
-mask = pat.gp('reg_mask')
-output_folder = pat.gp('reg')
-
-def registration(moved_image,fixed_image,mask,output_folder):
 	
-	prefix='%s_to_%s'%(os.path.basename(moved_image).split('.')[0],os.path.basename(fixed_image).split('.')[0])
-	print prefix
-	registration_parametrs=['-d', '3',
-							'--transform', 'Affine[0.75]',
-							'--metric',  'MI[%s,%s,0.5,32]'%(fixed_image,moved_image),
-							'--convergence', '[100x100x100,1e-6,5]',
-							'--shrink-factors', '8x2x1',
-							'--smoothing-sigmas', '6x2x2vox',
-							'--use-estimate-learning-rate-once',
-							'-x %s'%mask,
+	pat.add_path(fn,'filtered',s,a,add_to=(pat.get_path('filtered',4)))
+pat.print_all()
 
-							'--transform', 'SyN[0.75]',
-							'--metric',  'MI[%s,%s,0.5,32]'%(fixed_image,moved_image),
-							'--convergence', '[100x100x100,1e-6,5]',
-							'--shrink-factors', '6x2x1',
-							'--smoothing-sigmas', '8x2x1vox',
-							'--use-estimate-learning-rate-once'
-							'-x %s'%mask,
-							'-o',prefix
-							]
-	#print ' '.join(registration_parametrs)
-	subprocess.check_call('ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=3',shell=True)
-	subprocess.check_call(' '.join([ANTs_PATH+'antsRegistration', ' '.join(registration_parametrs)]),shell=True,cwd=output_folder)
-	subprocess.check_call(' '.join([ANTs_PATH+'antsApplyTransforms',
-							   '-d 3',
-							   '-r',fixed_image,
-							   '-i',moved_image,
-							   '-t [%s0GenericAffine.mat,0]'%prefix,
-							   '-t [%s1Warp.nii.gz,0]'%prefix,
-							   '-o %s_registered.nii.gz'%prefix])
-					,shell=True,cwd=output_folder)
+def registration_start():
+	ANTs_PATH = '/home/denest/ANTs-1.9.x-Linux/bin/'
+	#registration
+	WORKING_FOLDER = pat.get_path('root')
+	#images_folder = pat.get_path('filtered,4')
+	fixed_im=pat.get_path('filtered',4,TARGET_PHASE)
+	#moved_im='20140508_100402GeneralBodyPerfusionCopiedTEMNOSAGATYIAV02041973s004a001_20_I40_G1.5-subvolume-scale_1.nii.gz'
+	mask = pat.get_path('registration_roi')
+	print mask
+	output_folder = pat.get_path('reg',4)
 
-for aq_n,file_name in pat.gp('filtered',4):
-	if not aq_n:
-		continue
-	fname = os.path.join(images_folder,file_name)
-	if not fname==os.path.join(images_folder ,fixed_im):
-		registration(os.path.relpath(fname,output_folder),os.path.relpath(fixed_im,output_folder),os.path.relpath(mask,output_folder),output_folder)
+	def registration(moved_image,fixed_image,mask,output_folder):
+		
+		prefix='%s_to_%s'%(os.path.basename(moved_image).split('.')[0],os.path.basename(fixed_image).split('.')[0])
+		print prefix
+		fixed_image=os.path.relpath(fixed_image,output_folder)
+		moved_image=os.path.relpath(moved_image,output_folder)
+		registration_parametrs=['-d', '3',
+								'--transform', 'Affine[0.75]',
+								'--metric',  'MI[%s,%s,0.5,32]'%(fixed_image,moved_image),
+								'--convergence', '[100x100x0,1e-6,5]',
+								'--shrink-factors', '8x2x1',
+								'--smoothing-sigmas', '6x2x2vox',
+								'--use-estimate-learning-rate-once',
+								'-x %s'%mask,
 
+								'--transform', 'SyN[0.75]',
+								'--metric',  'MI[%s,%s,0.5,32]'%(fixed_image,moved_image),
+								'--convergence', '[100x100x0,1e-6,5]',
+								'--shrink-factors', '6x2x1',
+								'--smoothing-sigmas', '8x2x1vox',
+								'--use-estimate-learning-rate-once'
+								'-x %s'%mask,
+								'-o',prefix
+								]
+		#print ' '.join(registration_parametrs)
+		subprocess.check_call('ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=3',shell=True)
+		subprocess.check_call(' '.join([ANTs_PATH+'antsRegistration', ' '.join(registration_parametrs)]),shell=True,cwd=output_folder)
+		subprocess.check_call(' '.join([ANTs_PATH+'antsApplyTransforms',
+								   '-d 3',
+								   '-r',fixed_image,
+								   '-i',moved_image,
+								   '-t [%s0GenericAffine.mat,0]'%prefix,
+								   '-t [%s1Warp.nii.gz,0]'%prefix,
+								   '-o %s_registered.nii.gz'%prefix])
+						,shell=True,cwd=output_folder)
+
+	registration(pat.get_path('filtered',4,10),fixed_im,mask,output_folder)
+	"""
+	for aq_n,file_name in pat.gp('filtered',4):
+		if not aq_n:
+			continue
+		fname = os.path.join(images_folder,file_name)
+		if not fname==os.path.join(images_folder ,fixed_im):
+			registration(os.path.relpath(fname,output_folder),os.path.relpath(fixed_im,output_folder),os.path.relpath(mask,output_folder),output_folder)
+	"""
 
 
 """
@@ -211,13 +240,14 @@ for aq_n,file_name in pat.gp('filtered',4):
 #calculte rois parametrs
 
 """
-roi.add('aorta',pat.gp('aorta'))
-roi.add('ivc',pat.gp('ivc'))
+def roi_calculation():
+	roi.add('aorta',pat.gp('aorta'))
+	roi.add('ivc',pat.gp('ivc'))
 
-roi.add('pancreas',pat.gp('pancreas'))
-for i,t in zip(range(21),time_list):
-	for roi_name in ("aorta","ivc","pancreas","tumor"):
-		roi.add_vol(roi_name,pat.gp(roi_name,4,i),t)
+	roi.add('pancreas',pat.gp('pancreas'))
+	for i,t in zip(range(21),time_list):
+		for roi_name in ("aorta","ivc","pancreas","tumor"):
+			roi.add_vol(roi_name,pat.gp(roi_name,4,i),t)
 
 """
 Subfolder structure
@@ -286,3 +316,10 @@ tumor
 """
 roi_name;vol_path,vol_time....
 """
+
+if __name__ == "__main__":
+	#make_root_dirs()
+	#convert_dcm_to_nii()
+	#filter_vols()
+	registration_start()
+	pass
